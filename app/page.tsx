45eb55491fb6f56f540228d10d75fc0fb9f8a199
Arjun Manjunath <arjunmanjunath80@gmail.com>
2025-04-07 23:03:17 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,19 @@ import Main from 'app/Main'
 export default async function Page() {
   const sortedPosts = sortPosts(allBlogs)
   const posts = allCoreContent(sortedPosts)
-  // return <Main posts={posts} />
-  return (
-    <div className="text-center">
-      <h1 className="mb-4 text-5xl font-bold">My Blog</h1>
-      <p className="mb-6 text-lg text-gray-400">
-        A space for my thoughts, ideas, and stories. Coming soon!
-      </p>
-      <button className="rounded-lg bg-blue-600 px-6 py-3 font-semibold text-white transition hover:bg-blue-500">
-        Notify Me
-      </button>
-    </div>
+  return <>
+    <Main posts={posts} />
+    <iframe src="https://unity.arjunmnath.me" width="600" height="400" frameborder="0" allowfullscreen></iframe>
+  </> 
+  // return (
+  //   <div className="text-center">
+  //     <h1 className="mb-4 text-5xl font-bold">My Blog</h1>
+  //     <p className="mb-6 text-lg text-gray-400">
+  //       A space for my thoughts, ideas, and stories. Coming soon!
+  //     </p>
+  //     <button className="rounded-lg bg-blue-600 px-6 py-3 font-semibold text-white transition hover:bg-blue-500">
+  //       Notify Me
+  //     </button>
+  //   </div>
   )
 }
